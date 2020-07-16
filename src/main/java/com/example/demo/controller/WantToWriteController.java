7accdb6544f242c40f0d00b2cc1638af72e6c3f9
ratelGloria <1007227291@qq.com>
2020-07-16 18:26:50 +0800

    upload img

--- src/main/java/com/example/demo/controller/WantToWriteController.java
+++ src/main/java/com/example/demo/controller/WantToWriteController.java
@@ -7,11 +7,16 @@ import com.example.demo.pojo.Blogs;
 import com.example.demo.pojo.User;
 import com.example.demo.redis.IRedisServiceImp;
 import com.example.demo.service.BlogService;
+import io.netty.handler.codec.base64.Base64Decoder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import sun.misc.BASE64Decoder;
 
 import javax.servlet.http.HttpSession;
 import javax.websocket.server.PathParam;
+import java.io.IOException;
+import java.util.Base64;
+import java.util.HashMap;
 
 @CrossOrigin
 @RestController
@@ -58,6 +63,81 @@ public class WantToWriteController {
         }
         return ServerResponse.serverResponseUnSuccess("请登录");
 //无知使我对这个世界充满好奇
+    }
+
+    @RequestMapping("addImg")
+    public ServerResponse addImg(@RequestParam(value = "imgUrl",required = false)String imgUrl){
+        System.out.println("--------ooo-----"+imgUrl);
+        return null;
+    }
+
+    @RequestMapping("addImgMap")
+    public ServerResponse addImgMap(@RequestParam HashMap map){
+        System.out.println("--------ooo-----"+map.toString());
+        BASE64Decoder base64Decoder = new BASE64Decoder();
+        byte[] imgUrls = new byte[0];
+        try {
+            imgUrls = base64Decoder.decodeBuffer((String) map.get("imgUrl"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("--------ooo-----"+imgUrls.toString());
+        return null;
+
+
+        /*
+        * <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link type="text/css" rel="stylesheet" href="../css/testPage.css">
+    <script src="https://cdn.bootcss.com/jquery/3.4.1/jquery.js"></script>
+</head>
+<body>
+<form enctype="multipart/form-data">
+    <input type="file" id="img">
+    <div id="div1"><img src=""></div>
+
+    <div style="width: 60px;height: 30px;background-color: aqua" id="commit">走你</div>
+</form>
+
+</body>
+
+<script>
+
+    $(function () {
+
+        $("#commit").on("click",function () {
+console.log($("#div1 img").attr("src"))
+            $.ajax({
+                url:"http://192.168.1.122:8051/WantToWrite/addImgMap",
+                type:"post",
+                dataType:"json",
+                // contentType: 'application/json; charset=utf-8',
+                data:{"imgUrl":$("#div1 img").attr("src")},
+
+            })
+        })
+
+        $("#img").on("change",function (evt) {
+            var c = evt.target.files[0]
+            // var c = evt.files[0];
+
+            var reader = new FileReader();
+            reader.onload = function(a){
+                var w = a.target.result;
+                $("#div1 img").attr("src",w)
+            }
+            reader.readAsDataURL(c);
+        })
+    })
+
+</script>
+
+</html>
+        * */
     }
 
     /*
